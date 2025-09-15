e501ee78c29d23c91f94907e74f559d01b39f062
youngrae0317 <145316807+youngrae0317@users.noreply.github.com>
2025-09-15 09:18:14 +0900

    [level 2] Title: 피보나치 수, Time: 4.25 ms, Memory: 81.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n) {
+        int [] fib = new int [n+1];
+        
+        fib[0] = 0;
+        fib[1] = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            
+            fib[i] = (fib[i-2] + fib[i-1]) % 1234567;
+        }
+        
+        return fib[n];
+    }
+}
